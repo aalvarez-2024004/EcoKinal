ecafba486f8694721370835f617d708f33fb0634
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-02-24 21:35:24 -0600

    Se agrego el modelo de la imagen a clasificar

---
+++ DetectorDeReciclaje/src/ClasificacionImagen/clasificacion.model.js
@@ -0,0 +1,27 @@
+'use strict'
+import mongoose, { Schema, model } from "mongoose";
+
+const clasificacionSchema = mongoose.Schema({
+    imagen:{
+        type:String,
+        required: [true, 'Es necesario que añadas una imagen para clasificar en que lugar va']
+    },
+    labels:{
+        type:[String],
+        default: []
+    },
+    tipo:{
+        type: String,
+        required: true
+    },
+    contenedor: {
+        type: String,
+        required: true
+    },
+    fecha: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export default mongoose.model('Clasificacion', clasificacionSchema);
