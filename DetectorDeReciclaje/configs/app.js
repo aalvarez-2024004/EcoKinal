59392d33262e6a887b8f7d8fe91ad675de5802e8
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-02-24 20:15:51 -0600

    Se configuro la conexión con la base de datos y el app.js

---
+++ DetectorDeReciclaje/configs/app.js
@@ -0,0 +1,24 @@
+'use strict';
+ 
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+ 
+ 
+const BASE_PATH = '/DetectorImagenReciclaje/v1';
+ 
+export const createApp = () => {
+    const app = express();
+ 
+    // Middlewares
+    app.use(express.json());
+    app.use(cors());
+    app.use(helmet());
+ 
+    // Ruta de prueba
+    app.get(`${BASE_PATH}/health`, (req, res) => {
+        res.json({ message: 'API DetectorReciclaje corriendo ' });
+    });
+ 
+    return app;
+};
