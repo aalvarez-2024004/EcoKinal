addf91d95cc2efdea40ba64b0e1776f79120d6cf
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2026-03-03 21:16:41 -0600

    Se el helper para que al usuario le llegue el correo con el token para verificar que es el

--- AuthEcoKinal/helpers/send-email.js
+++ AuthEcoKinal/helpers/send-email.js
@@ -24,3 +24,30 @@ const verificationLink = `${token}`
     `
   })
 }
+
+export const sendResetPasswordEmail = async (email, token) => {
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  })
+
+  const resetLink = `http://localhost:3000/api/auth/reset-password?token=${token}`
+
+  await transporter.sendMail({
+    from: `"Autenticacion ECO KINAL" <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: 'Recuperación de contraseña',
+    html: `
+      <h2>Restablecer contraseña</h2>
+      <p>Haz clic en el siguiente enlace para cambiar tu contraseña:</p>
+      <a href="${resetLink}">${resetLink}</a>
+      <br><br>
+      <p>Este enlace expirará en 30 minutos.</p>
+      <p>Si no solicitaste este cambio, ignora este correo.</p>
+    `
+  })
+}
